3e0b887988deecb289bf79e127f2590631963134
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2024-10-19 00:23:07 +0530

    Runtime: 0 ms (100.00%), Memory: 41.4 MB (91.34%) - Gitcode

--- 66-plus-one/66. Plus One.java
+++ 66-plus-one/66. Plus One.java
@@ -1,13 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for(int i = digits.length-1;i>=0;i--){
-            if(digits[i] + 1 != 10){
-                digits[i] += 1;
-                return digits;
-            }
-            digits[i] = 0;
+       int n = digits.length;
+       for(int i = n-1; i>=0;i--){
+        if(digits[i]<9){
+            digits[i]++;
+            return digits;
         }
-        int[] newDig = new int[digits.length+1];
+        digits[i]=0;
+       }
+
+        int[] newDig = new int[n+1];
         newDig[0] = 1;
         return newDig; 
     }
