4877839797b6b62cc73e2150d74068de89a4b1e4
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-05-15 20:38:05 +0530

    Time: 7 ms (55.49%) | Memory: 51.2 MB (88.43%) - LeetSync

---
+++ 89-gray-code/gray-code.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> list= new ArrayList<>();
+
+        for(int i=0;i<Math.pow(2,n);i++)
+        {
+            list.add(i^(i>>>1));
+            
+        }
+        return list;
+        
+    }
+}
