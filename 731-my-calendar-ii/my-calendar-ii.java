41270ead302fe56c9992eba65e27fe72076fea82
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-05-19 22:02:46 +0530

    Time: 89 ms (53.07%) | Memory: 45.5 MB (90.79%) - LeetSync

---
+++ 731-my-calendar-ii/my-calendar-ii.java
@@ -0,0 +1,44 @@
+class MyCalendarTwo {
+    private List<int[]> bookings;
+
+    public MyCalendarTwo() {
+        bookings = new ArrayList<>();
+    }
+    
+    public boolean book(int start, int end) {
+        for (int[] interval : bookings) {
+            int a = interval[0], b = interval[1];
+
+            if (start < b && end > a) {
+                int newStart = Math.max(a, start);
+                int newEnd = Math.min(b, end);
+
+                if (check(newStart, newEnd)) {
+                    return false;  
+                }
+            }
+        }
+
+        bookings.add(new int[] {start, end});
+        return true;  
+    }
+
+   
+    private boolean check(int start, int end) {
+        int overlapCount = 0;
+
+        for (int[] interval : bookings) {
+            int a = interval[0], b = interval[1];
+
+            if (start < b && end > a) {
+                overlapCount++;
+                if (overlapCount == 2) {
+                    return true;  
+                }
+            }
+          
+        }
+
+        return false; 
+    }
+}
