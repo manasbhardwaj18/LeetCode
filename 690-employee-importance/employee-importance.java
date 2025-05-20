f2f73c74a6faf300b67ccc697f9feb340de9fdb9
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-05-20 23:55:00 +0530

    Time: 4 ms (99.21%) | Memory: 46.6 MB (36.71%) - LeetSync

---
+++ 690-employee-importance/employee-importance.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    public int dfs (int id, Map<Integer, Employee> map) {
+        
+        int answer = map.get (id).importance;
+        
+        for (int newId : map.get (id).subordinates) {
+            answer += dfs (newId, map);
+        }
+        
+        return answer;
+    }
+    
+    public int getImportance(List<Employee> employees, int id) {
+        
+        Map<Integer, Employee> map = new HashMap<> ();
+        
+        for (Employee employee : employees) {
+            map.put (employee.id, employee);
+        }
+        
+        return dfs (id, map);
+    }
+}
