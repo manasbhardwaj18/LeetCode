5e6fcc11afc0abc743c35fe24ac7beb7368430a6
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-05-22 10:24:13 +0530

    Time: 1 ms (72.87%) | Memory: 45.8 MB (29.93%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,30 +1,38 @@
 class Solution {
-public void setZeroes(int[][] matrix) {
-    boolean fr = false,fc = false;
-    for(int i = 0; i < matrix.length; i++) {
-        for(int j = 0; j < matrix[0].length; j++) {
-            if(matrix[i][j] == 0) {
-                if(i == 0) fr = true;
-                if(j == 0) fc = true;
-                matrix[0][j] = 0;
-                matrix[i][0] = 0;
+    public void setZeroes(int[][] matrix) {
+        List<List<Integer>> zeroPos = new ArrayList<>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = 0;i<m;i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    List<Integer> dummy = Arrays.asList(i, j);
+                    zeroPos.add(dummy);
+                }
             }
         }
-    }
-    for(int i = 1; i < matrix.length; i++) {
-        for(int j = 1; j < matrix[0].length; j++) {
-            if(matrix[i][0] == 0 || matrix[0][j] == 0) {
-                matrix[i][j] = 0;
-        }}
-    }
-    if(fr) {
-        for(int j = 0; j < matrix[0].length; j++) {
-            matrix[0][j] = 0;
+
+        int sol = zeroPos.size();
+        int start, end;
+
+        for (int i = 0;i<sol;i++) {
+            start = zeroPos.get(i).get(0);
+            end = zeroPos.get(i).get(1);
+            MakeRowZeroes(matrix[start],n);
+            MakeColumnZeroes(matrix,m,end);
         }
     }
-    if(fc) {
-        for(int i = 1; i < matrix.length; i++) {
-            matrix[i][0] = 0;
+
+    void MakeRowZeroes(int[] arr,int n) {
+        for (int i = 0;i<n;i++) {
+            arr[i] = 0;
         }
     }
-}}  
+
+    void MakeColumnZeroes(int[][] arr,int m,int index) {
+        for (int i = 0;i < m;i++) {
+            arr[i][index] = 0;
+        }
+    }
+}
