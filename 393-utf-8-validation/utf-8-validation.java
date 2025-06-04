976e9d01dec5ebf20d1c7a9104bc8a912602c0e4
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-06-04 22:51:30 +0530

    Time: 1 ms (98.28%) | Memory: 45 MB (32.99%) - LeetSync

---
+++ 393-utf-8-validation/utf-8-validation.java
@@ -0,0 +1,34 @@
+class Solution {
+	public boolean validUtf8(int[] data) {
+		int remaining=0;
+		for(int i:data){
+			if(remaining==0){
+				if(i>>7==0b0){
+					remaining = 0;
+				}
+				else if(i>>5==0b110){
+					remaining = 1;
+				}
+				else if(i>>4==0b1110){
+					remaining = 2;
+				}
+				else if(i>>3==0b11110){
+					remaining = 3;
+				}
+				else{
+					return false;
+				}
+			}
+			else{
+				if(i>>6==0b10){
+					remaining--;
+				}
+				else{
+					return false;
+				}
+			}
+		}
+		return remaining==0?true:false;
+
+	}
+}
