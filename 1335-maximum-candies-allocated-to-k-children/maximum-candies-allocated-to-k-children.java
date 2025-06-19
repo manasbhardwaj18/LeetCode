e84060ef3695eb6c77727a647c4aa3f725f6779c
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-06-19 15:01:47 +0530

    Time: 25 ms (71.68%) | Memory: 63.3 MB (30.35%) - LeetSync

---
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maximumCandies(int[] candies, long k) {
+        int left = 1, right = 0;
+        for (int c : candies) right = Math.max(right, c); 
+        long total = 0;
+        for (int c : candies) total += c;
+        if (total < k) return 0; 
+        int result = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (canDistribute(candies, k, mid)) {
+                result = mid;
+                left = mid + 1; 
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    private boolean canDistribute(int[] candies, long k, int val) {
+        long count = 0; 
+        for (int c : candies) {
+            count += c / val;
+            if (count >= k) return true;  
+        }
+        return false;
+    }
+}
