eadde8ce431bf989e1920697f8814f8da5b30765
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-07-29 11:14:50 +0530

    Time: 58 ms (52.27%) | Memory: 46.8 MB (53.65%) - LeetSync

---
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        Map<String, List<String>> graph = new HashMap<>(); 
+        Map<String, Integer> indegree = new HashMap<>();   
+        Set<String> available = new HashSet<>(Arrays.asList(supplies));
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < recipes.length; i++) {
+            String recipe = recipes[i];
+            indegree.put(recipe, ingredients.get(i).size());
+
+            for (String ing : ingredients.get(i)) {
+                graph.computeIfAbsent(ing, k -> new ArrayList<>()).add(recipe);
+            }
+        }
+
+        Queue<String> queue = new LinkedList<>(available);
+
+        while (!queue.isEmpty()) {
+            String item = queue.poll();
+            if (!graph.containsKey(item)) continue;
+
+            for (String recipe : graph.get(item)) {
+                indegree.put(recipe, indegree.get(recipe) - 1);
+                if (indegree.get(recipe) == 0) {
+                    result.add(recipe);
+                    queue.add(recipe);
+                }
+            }
+        }
+
+        return result;
+    }
+}
