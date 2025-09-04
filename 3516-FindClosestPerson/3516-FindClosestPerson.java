e81961bf5d10592ba3e86850f1edd7da0cd3e8fd
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-09-04 13:07:47 +0530

    Create 3516-FindClosestPerson

---
+++ 3516-FindClosestPerson/3516-FindClosestPerson.java
@@ -0,0 +1,11 @@
+// Last updated: 9/4/2025, 1:07:48 PM
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int d1 = Math.abs(z - x);
+        int d2 = Math.abs(z - y);
+
+        if (d1 < d2) return 1;
+        else if (d2 < d1) return 2;
+        else return 0;
+    }
+}
