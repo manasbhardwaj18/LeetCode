b241d63c0c760c9e0464889f61b7df683142e55f
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-09-15 12:38:04 +0530

    Create 1935-MaximumNumberofWordsYouCanType

---
+++ 1935-MaximumNumberofWordsYouCanType/1935-MaximumNumberofWordsYouCanType.java
@@ -0,0 +1,24 @@
+// Last updated: 9/15/2025, 12:38:04 PM
+class Solution {
+    public int canBeTypedWords(String text, String broken) {
+        Set<Character> brokenKeys = new HashSet<>();
+        int count = 0;
+
+        for (char c : broken.toCharArray()) {
+            brokenKeys.add(c);
+        }
+
+        String[] words = text.split(" ");
+
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                if (brokenKeys.contains(c)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return words.length - count;
+    }
+}
