df8aef4844e0ce5ac4446a051a2d2350a63d3af5
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-10-16 12:42:10 +0530

    Create 2598-SmallestMissingNon-negativeIntegerAfterOperations

---
+++ 2598-SmallestMissingNon-negativeIntegerAfterOperations/2598-SmallestMissingNon-negativeIntegerAfterOperations.java
@@ -0,0 +1,17 @@
+// Last updated: 10/16/2025, 12:42:09 PM
+class Solution {
+    public int findSmallestInteger(int[] nums, int value) {
+        int n = nums.length;
+        int result = 0;
+        int[] rem = new int[value];
+        for(int x : nums){
+            int r = ( (x % value ) + value ) % value;
+            rem[r]++;
+        }
+       while( rem [result % value] > 0){
+        rem[result%value]--;
+        result++;
+       }
+       return result;
+    }
+}
