99ba5319e781544bfe2e8bfac908b9bf765f3c5e
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-10-21 13:55:41 +0530

    File updated at 10/21/2025, 1:55:42 PM

--- 3346-MaximumFrequencyofanElementAfterPerformingOperationsI/3346-MaximumFrequencyofanElementAfterPerformingOperationsI.java
+++ 3346-MaximumFrequencyofanElementAfterPerformingOperationsI/3346-MaximumFrequencyofanElementAfterPerformingOperationsI.java
@@ -1,24 +1,36 @@
-// Last updated: 10/21/2025, 1:54:59 PM
+// Last updated: 10/21/2025, 1:55:42 PM
 class Solution {
-    public int maxFrequency(int[] nums, int k, int ops) {
-        int mx = Arrays.stream(nums).max().getAsInt();
-        int n = mx + k + 2;
-        int[] f = new int[n];
-        for (int x : nums) f[x]++;
+    public int maxFrequency(int[] nums, int k, int numOperations) {
+        int n = nums.length, ans = 0, left = 0, right = 0;
+        Arrays.sort(nums);
 
-        int[] pre = new int[n];
-        pre[0] = f[0];
-        for (int i = 1; i < n; i++) pre[i] = pre[i - 1] + f[i];
+        HashMap<Integer, Integer> count = new HashMap<>();
+        for(int num : nums) count.put(num, count.getOrDefault(num, 0) + 1);
 
-        int ans = 0;
-        for (int t = 0; t < n; t++) {
-            if (f[t] == 0 && ops == 0) continue;
-            int l = Math.max(0, t - k), r = Math.min(n - 1, t + k);
-            int tot = pre[r] - (l > 0 ? pre[l - 1] : 0);
-            int adj = tot - f[t];
-            int val = f[t] + Math.min(ops, adj);
-            ans = Math.max(ans, val);
+        for(int mid = 0; mid < n; mid++) {
+            while(nums[mid] - nums[left] > k) {
+                left++;
+            }
+
+            while(right < n - 1 && nums[right + 1] - nums[mid] <= k) {
+                right++;
+            }
+
+            int total = right - left + 1;
+            ans = Math.max(ans, Math.min(total - count.get(nums[mid]), numOperations) + count.get(nums[mid]));
         }
+
+        left = 0;
+        for(right = 0; right < n; right++) {
+            int mid = (nums[left] + nums[right]) / 2;
+
+            while(mid - nums[left] > k || nums[right] - mid > k) {
+                left++;
+                mid = (nums[left] + nums[right]) / 2;
+            }
+            ans = Math.max(ans, Math.min(right - left + 1, numOperations));
+        }
+
         return ans;
     }
 }
