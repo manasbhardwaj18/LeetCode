2e728820e2447e5aa9db0b0c965129f5dc724b21
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-11-04 11:41:06 +0530

    File updated at 11/4/2025, 11:41:08 AM

--- 3318-FindX-SumofAllK-LongSubarraysI/3318-FindX-SumofAllK-LongSubarraysI.java
+++ 3318-FindX-SumofAllK-LongSubarraysI/3318-FindX-SumofAllK-LongSubarraysI.java
@@ -1,46 +1,30 @@
-// Last updated: 11/4/2025, 11:40:49 AM
+// Last updated: 11/4/2025, 11:41:08 AM
 class Solution {
-    HashMap<Integer, Integer> hmap;
     public int[] findXSum(int[] nums, int k, int x) {
-        hmap = new HashMap<>();
-        int n = nums.length;
-        int ans[] = new int[n-k+1];
-        int index = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int [] res = new int [nums.length - k + 1];
 
-        for(int i=0; i<n; i++) {
-            hmap.put(nums[i], hmap.getOrDefault(nums[i], 0) + 1);
-            if(i >= k-1) {
-                ans[index++] = getSum(x);
-                int removenum = nums[i-k+1];
-                hmap.put(removenum, hmap.get(removenum)-1);
-                if(hmap.get(removenum)==0) hmap.remove(removenum);
+        for (int i = 0, j = 0, r = 0; j < nums.length; j++) {
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+
+            if (j >= k - 1) {
+                Queue<int []> q = new PriorityQueue<>((a,b) -> b[1] == a[1] ? b[0] - a[0] : b[1] - a[1]);
+                for (int key : map.keySet()) {
+                    q.add(new int [] {key, map.get(key)});
+                }
+
+                int m = x, sum = 0;
+
+                while (!q.isEmpty() && m-- > 0) {
+                    int [] a = q.remove();
+                    sum += a[0] * a[1];
+                }
+
+                res[r++] = sum;
+                map.put(nums[i], map.get(nums[i++]) - 1);
             }
-
-        }
-        return ans;
-    }
-
-    int getSum(int x) {
-        int sum = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> {
-            {
-                int f1 = hmap.get(a);
-                int f2 = hmap.get(b);
-                if(f1==f2) 
-                    return b-a;
-                return f2-f1;
-            }
-        });
-
-        for(int key : hmap.keySet()) {
-            pq.offer(key); 
         }
 
-        while(!pq.isEmpty() && x>0) {
-            int top = pq.remove();
-            sum += top*hmap.get(top);
-            x--;
-        }
-        return sum;
+        return res;
     }
 }
