9a443c91610c751ba9bd450feb9863ec0d906746
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-11-08 18:07:22 +0530

    File updated at 11/8/2025, 6:07:22 PM

--- 1611-MinimumOneBitOperationstoMakeIntegersZero/1611-MinimumOneBitOperationstoMakeIntegersZero.java
+++ 1611-MinimumOneBitOperationstoMakeIntegersZero/1611-MinimumOneBitOperationstoMakeIntegersZero.java
@@ -1,11 +1,19 @@
-// Last updated: 11/8/2025, 6:07:03 PM
-class Solution {
+// Last updated: 11/8/2025, 6:07:22 PM
+public class Solution {
     public int minimumOneBitOperations(int n) {
-        int ans = 0;
-        while (n > 0) {
-            ans ^= n;   
-            n >>= 1;    
+        int ans = 0, f = 0;
+        for (int i = 31; i >= 0; i--) {
+            if (((n >> i) & 1) == 1) {
+                if (f == 0) {
+                    ans = ans + ((1 << (i + 1))) - 1;
+                    f = 1;
+                } else {
+                    ans = ans - ((1 << (i + 1)) - 1);
+                    f = 0;
+                }
+                System.out.println(ans);
+            }
         }
-        return ans;    
+        return ans;
     }
 }
