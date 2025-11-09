b3d9400ca91e5b72994714b6e766e43617b8c19b
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-11-09 12:24:47 +0530

    File updated at 11/9/2025, 12:24:47 PM

--- 2169-CountOperationstoObtainZero/2169-CountOperationstoObtainZero.java
+++ 2169-CountOperationstoObtainZero/2169-CountOperationstoObtainZero.java
@@ -1,17 +1,6 @@
-// Last updated: 11/9/2025, 12:24:37 PM
+// Last updated: 11/9/2025, 12:24:47 PM
 class Solution {
     public int countOperations(int num1, int num2) {
-        int count=0;
-        if(num1== 0 || num2 ==0){ return 0;}
-        while(num1 != num2){
-            if(num1>=num2){
-                num1-=num2;
-                count++;
-            }else{
-                num2-=num1;
-                count++;
-            }
-        }
-        return count+1;
+        return (num1 == 0 || num2 == 0) ? 0 : num1 / num2 + countOperations(num2, num1 % num2);
     }
 }
