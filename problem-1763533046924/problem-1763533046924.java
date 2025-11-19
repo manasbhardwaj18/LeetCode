5dd2f4634b43f5fdf9ca4c51e4c1643e6c6fcae0
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-11-19 11:47:25 +0530

    Create

---
+++ problem-1763533046924/problem-1763533046924.java
@@ -0,0 +1,28 @@
+// Last updated: 11/19/2025, 11:47:26 AM
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        Arrays.sort(nums);
+        while(true){
+            int left =0, right=nums.length-1;
+            boolean found = false;
+
+            while(left<=right){
+                int mid = left+(right-left)/2;
+                if(nums[mid] == original){
+                    found = true;
+                    break;
+                }else if(nums[mid]<original){
+                    left = mid +1;
+                }else{
+                    right = mid - 1;
+                }
+            }
+            if(found){
+                original *= 2;
+            }else{
+                break;
+            }
+        }
+        return original;
+    }
+}
