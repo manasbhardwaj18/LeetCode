b207eacf28756d3ec79e5f8e71a179a4283dbef4
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-11-24 15:43:20 +0530

    File updated at 11/24/2025, 3:43:21 PM

--- 1018-BinaryPrefixDivisibleBy5/1018-BinaryPrefixDivisibleBy5.java
+++ 1018-BinaryPrefixDivisibleBy5/1018-BinaryPrefixDivisibleBy5.java
@@ -1,12 +1,13 @@
-// Last updated: 11/24/2025, 3:42:34 PM
+// Last updated: 11/24/2025, 3:43:21 PM
 class Solution {
     public List<Boolean> prefixesDivBy5(int[] nums) {
         List<Boolean> result = new ArrayList<>();
         int msb=0;
         for(int x : nums){
-            msb = ((msb<<1)+x)%5;
+            msb = ((msb*2)+x)%5;
             result.add(msb==0);
         }
         return result;
     }
 }
+
