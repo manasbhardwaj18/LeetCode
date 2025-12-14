d1369691a78868db7e815b78dcf6621dd30eb8f2
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2025-12-14 13:58:08 +0530

    File updated at 12/14/2025, 1:58:08 PM

--- 3000-MaximumAreaofLongestDiagonalRectangle/3000-MaximumAreaofLongestDiagonalRectangle.java
+++ 3000-MaximumAreaofLongestDiagonalRectangle/3000-MaximumAreaofLongestDiagonalRectangle.java
@@ -1,22 +1,19 @@
-// Last updated: 8/26/2025, 1:35:45 PM
-class Solution {
-    public int areaOfMaxDiagonal(int[][] dimensions) {
-        int maxArea=0;
-        int maxDia=0;
-        int n=dimensions.length;
-        for(int i=0; i<n; i++) {
-            int l=dimensions[i][0];
-            int b=dimensions[i][1];
-
-            int currDia=l*l+b*b;
-            int currArea=l*b;
-
-            if(currDia>maxDia || (currDia==maxDia && currArea>maxArea)) {
-                maxArea=currArea;
-                maxDia=currDia;
-            }
-        }
-
-        return maxArea;
-    }
-}
+// Last updated: 12/14/2025, 1:58:08 PM
+1class Solution {
+2       public int intersectionSizeTwo(int[][] intervals) {
+3        int n = intervals.length;
+4        Arrays.sort(intervals, (a, b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]); 
+5        List<Integer> res = new ArrayList<>();
+6        res.add(intervals[0][1] - 1); 
+7        res.add(intervals[0][1]); 
+8        for (int i = 1; i < n; i++) { 
+9            int start = intervals[i][0], end = intervals[i][1], size = res.size(), last = res.get(size - 1), secondLast = res.get(size - 2);
+10            if (start > last) { 
+11                res.add(end - 1);
+12                res.add(end);
+13            } else if (start == last) res.add(end); 
+14            else if (start > secondLast) res.add(end); 
+15        }
+16        return res.size();
+17    }
+18}
