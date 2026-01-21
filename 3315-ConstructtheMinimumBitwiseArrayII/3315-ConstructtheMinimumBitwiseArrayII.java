ae2807509124932f4e5438d3e0a007ee5f3c6c61
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2026-01-21 13:27:16 +0530

    File updated at 1/21/2026, 1:27:15 PM

--- 3315-ConstructtheMinimumBitwiseArrayII/3315-ConstructtheMinimumBitwiseArrayII.java
+++ 3315-ConstructtheMinimumBitwiseArrayII/3315-ConstructtheMinimumBitwiseArrayII.java
@@ -1,29 +1,21 @@
-// Last updated: 1/21/2026, 1:26:21 PM
-1class Solution {
-2    public int[] minBitwiseArray(List<Integer> nums) {
-3        int n = nums.size();
-4        int[] res = new int[n];
-5        Arrays.fill(res, -1);
-6
-7        for(int i =0; i<n; i++){
-8            if(nums.get(i) == 2){
-9                continue;
-10            }
-11            int a = nums.get(i);
-12            int pos = 0;
-13
-14            while(a > 0 && ((a >> pos ) & 1 ) == 1 ){
-15                pos++;
-16            }
-17
-18            if((1 << pos) > a){
-19                int topBit = 31 - Integer.numberOfLeadingZeros(a);
-20                a = a & ~(1 << topBit);
-21            }else{
-22                a = a & ~(1 << (pos - 1));
-23            }
-24            res[i] = a;
-25        }
-26        return res;
-27    }
-28}
+// Last updated: 1/21/2026, 1:27:15 PM
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+		int n = nums.size();
+		int[] ans = new int[n];
+		for (int i = 0; i < n; i++) {
+			ans[i] = findMinOr(nums.get(i));
+		}
+		return ans;
+	}
+
+	private int findMinOr(int num) {
+		if (num == 2)
+			return -1;
+		int i = 0;
+		while ((num & (1 << i)) != 0) {
+			i++;
+		}
+		return num ^ (1 << (i - 1));
+	}
+}
