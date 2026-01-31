44b39c7cf8e3bec3a15dbd9d54c56782ae4c6b5f
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2026-01-31 20:36:30 +0530

    Create

---
+++ problem-1769871990791/problem-1769871990791.java
@@ -0,0 +1,43 @@
+// Last updated: 1/31/2026, 8:36:30 PM
+1class Solution {
+2    public int minimumK(int[] nums) {
+3        int[] a1 = nums.clone();
+4        long total = 0;
+5        for(int x : a1){
+6            total += x;
+7        }
+8        int lf = (int) Math.cbrt(total);
+9        if((long) lf*lf*lf < total){
+10            lf++;
+11        }
+12        int rt = (int)Math.sqrt(total) + 1;
+13
+14        
+15        
+16        while(lf < rt){
+17            int mid = lf + (rt - lf) / 2;
+18            if(op(a1, mid) <= (long) mid*mid){
+19                rt = mid;
+20            }else{
+21                lf = mid + 1;
+22            }
+23        }
+24        return lf;
+25    }
+26   int mmax(int[] nums){
+27       int m = 0;
+28       for(int x : nums){
+29           m = Math.max(m,x);
+30       }
+31       return m;
+32   }
+33
+34    long op(int[] nums,int k){
+35        long ops = 0;
+36        for(int x : nums){
+37            ops += (x + k -1)/k;
+38        }
+39        return ops;
+40    }
+41}
+42
