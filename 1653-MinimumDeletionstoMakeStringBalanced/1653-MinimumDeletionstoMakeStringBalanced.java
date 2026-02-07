1af876d02f79120edda27018026aa74e09eaf683
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2026-02-07 12:29:51 +0530

    File updated at 2/7/2026, 12:29:52 PM

--- 1653-MinimumDeletionstoMakeStringBalanced/1653-MinimumDeletionstoMakeStringBalanced.java
+++ 1653-MinimumDeletionstoMakeStringBalanced/1653-MinimumDeletionstoMakeStringBalanced.java
@@ -1,17 +1,18 @@
-// Last updated: 2/7/2026, 12:26:20 PM
+// Last updated: 2/7/2026, 12:29:52 PM
 1class Solution {
 2    public int minimumDeletions(String s) {
 3        int ans = 0, bCnt = 0;
-4        for(int i=0;i < s.length(); i++){
-5            char c = s.charAt(i);
-6
-7            if(c == 'b'){
-8                bCnt++;
-9            }else if(bCnt > 0){
-10                ans++;
-11                bCnt--;
-12            }
-13        }
-14        return ans;
-15    }
-16}
+4        for(char c : s.toCharArray()){
+5            if(c == 'b'){
+6                bCnt++;
+7            }else{
+8                ans = Math.min(ans+1, bCnt);
+9            }
+10        }
+11        return ans;
+12    }
+13}
+14
+15
+16
+17
