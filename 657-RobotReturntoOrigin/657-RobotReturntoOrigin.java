fe613416f9b5fd07b5e10b65ea28f0ed732dcb32
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2026-04-05 11:33:37 +0530

    File updated at 4/5/2026, 11:33:38 AM

--- 657-RobotReturntoOrigin/657-RobotReturntoOrigin.java
+++ 657-RobotReturntoOrigin/657-RobotReturntoOrigin.java
@@ -1,20 +1,20 @@
-// Last updated: 9/21/2025, 9:23:50 PM
-class Solution {
-    public boolean judgeCircle(String move) {
-        int l=0, r=0, u=0, d=0;
-        
-        for(int i=0;i<move.length();i++){
-            if(move.charAt(i)=='L')
-               l++;
-            else if(move.charAt(i)=='R')
-               r++;  
-            else if(move.charAt(i)=='U')
-               u++; 
-            else
-               d++;
-        }
-        if(u==d&&l==r)
-           return true;
-        return false;
-    }
-}
+// Last updated: 4/5/2026, 11:33:38 AM
+1class Solution {
+2    public boolean judgeCircle(String move) {
+3        int l=0, r=0, u=0, d=0;
+4        
+5        for(int i=0;i<move.length();i++){
+6            if(move.charAt(i)=='L')
+7               l++;
+8            else if(move.charAt(i)=='R')
+9               r++;  
+10            else if(move.charAt(i)=='U')
+11               u++; 
+12            else
+13               d++;
+14        }
+15        if(u==d&&l==r)
+16           return true;
+17        return false;
+18    }
+19}
