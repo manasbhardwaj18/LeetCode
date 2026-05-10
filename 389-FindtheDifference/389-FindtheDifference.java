d8e178b9f685cc2171107b64f6bbe6ce9284c391
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2026-05-10 12:59:51 +0530

    File updated at 5/10/2026, 12:59:57 PM

--- 389-FindtheDifference/389-FindtheDifference.java
+++ 389-FindtheDifference/389-FindtheDifference.java
@@ -1,15 +1,14 @@
-// Last updated: 5/10/2026, 12:59:18 PM
+// Last updated: 5/10/2026, 12:59:57 PM
 1class Solution {
 2    public char findTheDifference(String s, String t) {
-3        char miss = 0; 
-4        int len = t.length();
-5
-6        for(int i = 0; i < len; i++) {  
-7            if(i < s.length()) {  
-8                miss ^= s.charAt(i);  
-9            }
-10            miss ^= t.charAt(i);  
-11        }
-12        return miss; 
-13    }
-14}
+3        int sumS = 0, sumT = 0;
+4        for (char c : s.toCharArray()) {
+5            sumS += c;
+6        }
+7
+8        for (char c : t.toCharArray()) {
+9            sumT += c;
+10        }
+11        return (char)(sumT - sumS);
+12    }
+13}
