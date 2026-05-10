1caf43ecca0d6198ae33ad9d1e736f2d83302145
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2026-05-10 10:56:30 +0530

    File updated at 5/10/2026, 10:56:36 AM

--- 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.java
+++ 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.java
@@ -1,15 +1,14 @@
-// Last updated: 5/10/2026, 10:55:51 AM
+// Last updated: 5/10/2026, 10:56:36 AM
 1class Solution {
 2    public int maxProfit(int[] prices) {
 3        int max = 0;
 4        int start = prices[0];
 5
-6        int len = prices.length;
-7
-8        for(int i = 1; i<len; i++){
-9            if(start < prices[i]) max += prices[i] - start;
-10            start = prices[i];
-11        }
-12        return max;
-13    }
-14}
+6
+7        for(int i = 1; i<prices.length; i++){
+8            if(start < prices[i]) max += prices[i] - start;
+9            start = prices[i];
+10        }
+11        return max;
+12    }
+13}
