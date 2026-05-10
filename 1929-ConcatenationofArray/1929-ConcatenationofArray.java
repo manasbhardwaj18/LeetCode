b427d0cf5cd2e62deade566f91b3a29aa481f9a0
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2026-05-10 13:20:02 +0530

    File updated at 5/10/2026, 1:20:08 PM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.java
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.java
@@ -1,13 +1,11 @@
-// Last updated: 5/10/2026, 1:14:39 PM
+// Last updated: 5/10/2026, 1:20:08 PM
 1class Solution {
-2    public int[] getConcatenation(int[] nums) {
-3        int n = nums.length;
-4        int[] ans = new int[2*n];
-5
-6        for(int i=0; i<n; i++){
-7            ans[i] = ans[i+n] = nums[i];
-8        }
-9
-10        return ans;
-11    }
-12}
+2    public int[] shuffle(int[] nums, int n) {
+3        int[] res = new int[2*n];
+4        for(int i=0; i<n; i++){
+5            res[2*i] = nums[i];
+6            res[2*i+1] = nums[i+n];
+7        }
+8        return res;
+9    }
+10}
