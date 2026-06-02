4c1603588e022eb377b5d891d826552e3ffb51f2
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2026-06-02 09:34:20 +0530

    File updated at 6/2/2026, 9:34:20 AM

--- 3633-EarliestFinishTimeforLandandWaterRidesI/3633-EarliestFinishTimeforLandandWaterRidesI.java
+++ 3633-EarliestFinishTimeforLandandWaterRidesI/3633-EarliestFinishTimeforLandandWaterRidesI.java
@@ -1,23 +1,22 @@
-// Last updated: 6/2/2026, 9:32:16 AM
+// Last updated: 6/2/2026, 9:34:20 AM
 1class Solution {
-2    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-3        int fin = Integer.MAX_VALUE;
-4        
-5        for(int i=0; i < landStartTime.length; i++){
-6            int time = landStartTime[i] + landDuration[i];
-7
-8            for(int j=0; j < waterStartTime.length; j++){
-9                fin = Math.min(fin, Math.max(time, waterStartTime[j]) + waterDuration[j]);
-10            }
-11        }
-12
-13        for(int i=0; i < waterStartTime.length; i++){
-14            int time = waterStartTime[i] + waterDuration[i];
-15
-16            for(int j=0; j < landStartTime.length; j++){
-17                fin = Math.min(fin, Math.max(time, landStartTime[j]) + landDuration[j]);
+2    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime,
+3        int[] waterDuration) {
+4            int n = landStartTime.length;
+5            int m = waterStartTime.length;
+6        
+7            int res = Integer.MAX_VALUE;
+8            for (int i = 0; i < n; i++) {
+9                for (int j = 0; j < m; j++) {
+10                    int land = landStartTime[i] + landDuration[i];
+11                    int land_water = Math.max(land, waterStartTime[j]) + waterDuration[j];
+12                    res = Math.min(res, land_water);
+13
+14                    int water = waterStartTime[j] + waterDuration[j];
+15                    int water_land = Math.max(water, landStartTime[i]) + landDuration[i];
+16                    res = Math.min(res, water_land);
+17                }
 18            }
-19        }
-20        return fin;
-21    }
-22}
+19        return res;
+20    }
+21}
