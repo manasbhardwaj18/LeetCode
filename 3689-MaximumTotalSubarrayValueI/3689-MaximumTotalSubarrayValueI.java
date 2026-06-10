6a6f5ab3064449fd236a61ba1f4361860dd2cb6d
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2026-06-10 15:38:31 +0530

    File updated at 6/10/2026, 3:38:32 PM

--- 3689-MaximumTotalSubarrayValueI/3689-MaximumTotalSubarrayValueI.java
+++ 3689-MaximumTotalSubarrayValueI/3689-MaximumTotalSubarrayValueI.java
@@ -1,4 +1,4 @@
-// Last updated: 6/9/2026, 12:04:56 PM
+// Last updated: 6/10/2026, 3:38:32 PM
 1class Solution {
 2    public long maxTotalValue(int[] nums, int k) {
 3        int min = nums[0], max = nums[0];
