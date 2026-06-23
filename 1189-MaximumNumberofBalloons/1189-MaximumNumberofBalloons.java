bc379602653fced0468d642e70c625e958b74b8f
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2026-06-23 14:25:11 +0530

    File updated at 6/23/2026, 2:25:12 PM

--- 1189-MaximumNumberofBalloons/1189-MaximumNumberofBalloons.java
+++ 1189-MaximumNumberofBalloons/1189-MaximumNumberofBalloons.java
@@ -1,16 +1,15 @@
-// Last updated: 6/23/2026, 2:22:12 PM
+// Last updated: 6/23/2026, 2:25:12 PM
 1class Solution {
 2    public int maxNumberOfBalloons(String text) {
-3       int b = 0, a= 0, l = 0, o = 0, n = 0;
-4
-5       for(char c : text.toCharArray()){
-6        if(c == 'b') b++;
-7        else if(c == 'a') a++;
-8        else if(c == 'l') l++;
-9        else if(c == 'o') o++;
-10        else if(c == 'n') n++;
-11       }
-12
-13       return Math.min(Math.min(b,a), Math.min(Math.min(l/2, o/2),n)); 
-14    }
-15}
+3        int count[] = new int[26];
+4        for(char c : text.toCharArray()){
+5            count[c - 'a']++;
+6        }
+7
+8        return Math.min(
+9            Math.min(count['b' - 'a'], count['a' - 'a']),
+10            Math.min(count['l'-'a']/2,
+11            Math.min(count['o' - 'a']/2, count['n' - 'a']))
+12        );
+13    }
+14}
