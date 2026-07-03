009d141d4f93bc13f1b8233508b94ed9a4950d13
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2026-07-03 13:22:37 +0530

    File updated at 7/3/2026, 1:22:37 PM

--- 3286-FindaSafeWalkThroughaGrid/3286-FindaSafeWalkThroughaGrid.java
+++ 3286-FindaSafeWalkThroughaGrid/3286-FindaSafeWalkThroughaGrid.java
@@ -1,49 +1,42 @@
-// Last updated: 7/3/2026, 1:18:43 PM
-1class Solution {
-2    public boolean findSafeWalk(List<List<Integer>> grid, int health) {
-3        int m = grid.size();
-4        int n = grid.get(0).size();
-5
-6        int[][] dist = new int[m][n];
-7        for (int[] row : dist)
-8            Arrays.fill(row, Integer.MAX_VALUE);
+// Last updated: 7/3/2026, 1:22:37 PM
+1class Tuple {
+2    int x, y, h; 
+3    Tuple(int i, int j, int h) {
+4        this.x = i; 
+5        this.y = j; 
+6        this.h = h; 
+7    }
+8}
 9
-10        Deque<int[]> dq = new ArrayDeque<>();
-11
-12        dist[0][0] = grid.get(0).get(0);
-13        dq.offerFirst(new int[]{0, 0});
-14
-15        int[] dx = {-1, 1, 0, 0};
-16        int[] dy = {0, 0, -1, 1};
-17
-18        while (!dq.isEmpty()) {
-19            int[] curr = dq.pollFirst();
-20            int x = curr[0];
-21            int y = curr[1];
-22
-23            if (x == m - 1 && y == n - 1)
-24                return dist[x][y] < health;
+10class Solution {
+11    public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+12        PriorityQueue<Tuple> pq = new PriorityQueue<>((a, b) -> b.h - a.h); 
+13
+14        pq.offer(new Tuple(0, 0, health - grid.get(0).get(0))); 
+15
+16        int[][] dirs = {
+17            {0, 1}, {1, 0}, {-1, 0}, {0, -1}
+18        }; 
+19        int m = grid.size(); 
+20        int n = grid.get(0).size(); 
+21        int rh[][] = new int[m][n]; // running health 
+22        rh[0][0] = health - grid.get(0).get(0); 
+23
+24        // for(int i = 0; i < m; i++) Arrays.fill(rh[i], -1); 
 25
-26            for (int k = 0; k < 4; k++) {
-27                int nx = x + dx[k];
-28                int ny = y + dy[k];
+26        while(pq.size() > 0) {
+27            Tuple cur = pq.remove(); // returns andn remove top elemenet  
+28            if(cur.x == m - 1 && cur.y == n - 1 ) return rh[m -1][n -1] > 0; 
 29
-30                if (nx < 0 || nx >= m || ny < 0 || ny >= n)
-31                    continue;
-32
-33                int w = grid.get(nx).get(ny);
-34
-35                if (dist[x][y] + w < dist[nx][ny]) {
-36                    dist[nx][ny] = dist[x][y] + w;
-37
-38                    if (w == 0)
-39                        dq.offerFirst(new int[]{nx, ny});
-40                    else
-41                        dq.offerLast(new int[]{nx, ny});
-42                }
-43            }
-44        }
-45
-46        return dist[m - 1][n - 1] < health;
-47    }
-48}
+30            for(int d[]: dirs) {
+31                int nx = cur.x + d[0], ny = cur.y + d[1], nh = rh[cur.x][cur.y]; 
+32                if(nx >= 0 && ny >= 0 && nx < m && ny < n && rh[nx][ny] < nh - grid.get(nx).get(ny)) {
+33                    pq.offer(new Tuple(nx, ny, nh - grid.get(nx).get(ny)));
+34                    rh[nx][ny] = nh - grid.get(nx).get(ny); 
+35                }
+36            }
+37        }
+38
+39        return false; 
+40    }
+41}
