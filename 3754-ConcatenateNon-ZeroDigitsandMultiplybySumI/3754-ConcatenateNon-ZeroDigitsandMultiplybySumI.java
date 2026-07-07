8f8c46a3c60d3133c6c34d1bf9b0d2227ed992b0
Manas Bhardwaj <133231315+manasbhardwaj18@users.noreply.github.com>
2026-07-07 13:16:16 +0530

    File updated at 7/7/2026, 1:16:16 PM

--- 3754-ConcatenateNon-ZeroDigitsandMultiplybySumI/3754-ConcatenateNon-ZeroDigitsandMultiplybySumI.java
+++ 3754-ConcatenateNon-ZeroDigitsandMultiplybySumI/3754-ConcatenateNon-ZeroDigitsandMultiplybySumI.java
@@ -1,13 +1,13 @@
-// Last updated: 7/7/2026, 1:00:05 PM
+// Last updated: 7/7/2026, 1:16:16 PM
 1class Solution {
 2    public long sumAndMultiply(int n) {
-3        long x = 0, sum = 0;
-4        for(char c : String.valueOf(n).toCharArray()){
-5            if(c != '0'){
-6                x = x*10 + c - '0';
-7                sum += c - '0';
-8            }
+3        long x = 0, sum = 0, len = 1;
+4        while(n != 0){
+5            x = (n % 10) * len + x;
+6            if(n % 10 != 0) len = len * 10;
+7            sum += n % 10;
+8            n = n/10;
 9        }
-10        return x * sum;
+10        return sum * x;
 11    }
 12}
